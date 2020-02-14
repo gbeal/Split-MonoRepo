1c969075d408f0d8da5c7d7a2a9d0572f7353436
Greg Beal <17188443+gbeal@users.noreply.github.com>
2020-02-14 08:59:31 -0500

    It actually works now...

--- Split-MonoRepo.ps1
+++ Split-MonoRepo.ps1
@@ -4,35 +4,39 @@ param (
     [string]$Destination = $(throw "-Destination is required."),
     [string]$AzureDevOpsInstance = $(throw "-AzureDevOpsInstance is required"),
     [string]$AzureDevOpsProject = $(throw "-AzureDevOpsProject is required"),
-    [System.Collections.Array]$ExcludedDirs = @("")
+    [System.Collections.ArrayList]$ExcludedDirs = @("")
 )
 
-if(-Not (Test-Path $SourceRepo))
-{
+Set-StrictMode -Version 3.0
+
+if (-Not (Test-Path $SourceRepo)) {
     throw "Source directory {$SourceRepo} does not exist"
 }
 
-if(-Not (Test-Path $SourceRepo/.git))
-{
+if (-Not (Test-Path $SourceRepo/.git)) {
     throw "Source directory {$SourceRepo} does not appear to be a git repository"
 }
 
-if(-Not (Test-Path $Destination))
-{
-    throw "Destination directory {$Destination} does not exist"
+if (-Not (Test-Path $Destination)) {
+    try {
+        New-Item -ItemType "Directory" $Destination
+    }
+    catch {
+        "Destination directory {$Destination} does not exist, and we couldn't create it"
+        throw
+    }
 }
 
 $charsToRemove = @( " ", "?", "~", "^", ":", "*", "[", "]", "@", "@{")
-$monoRepoDirs = [System.Collections.ArrayList](Get-ChildItem -Path $SourceRepo -Directory | Select-Object -Property Name)
+$monoRepoDirs = [System.Collections.ArrayList](Get-ChildItem -Path $SourceRepo -Directory | Select-Object -Property Name )
 $manyRepoDir = $Destination
 
 $monoRepoDirs.Remove($ExcludedDirs)
 
 Push-Location $SourceRepo
-foreach($dir in $monoRepoDirs)
-{
+foreach ($dir in $monoRepoDirs) {
     #tidy up the directory strings
-    $dir = $dir.Replace($charsToRemove, "")
+    $dir = $dir.Name.Replace($charsToRemove, "")
     $newDir = "$manyRepoDir/$dir"
     write-output "****$dir****"
 
@@ -47,22 +51,22 @@ foreach($dir in $monoRepoDirs)
 
     #init new git repo
     git init
+    #add remote to the monorepo
+    git remote add mono $SourceRepo
     #pull in new branch from above
-    git pull $dir $newBranchName
+    git pull mono $newBranchName
     #get the new repo ready to go
     Copy-Item  $SourceRepo/.gitignore .
     git add .
     git commit -m '***repo split.  Add .gitignore!'
 
-    #setup Azure Devops components
-
-    az repos create --project $AzureDevOpsProject --organziation $AzureDevOpsInstance --name $dir
+    #Create the Azure DevOps resources
+    az repos create --project $AzureDevOpsProject --organization "$AzureDevOpsInstance" --name $dir
+    #add remote to the repo you just created...
     git remote add origin $AzureDevOpsInstance/DefaultCollection/$AzureDevOpsProject/_git/$dir/
+    #push changes up to the remote
     git push origin -u master
 
     #pop back up to the $SourceRepo dir
     Pop-Location
-
-
 }
-
